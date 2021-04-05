7f1251c449cd20f30b6d455c18527318a363edf7
chj2788 <chj2788@gmail.com>
2021-04-05 17:51:04 +0900

    initialized Weatherapp project

---
+++ src/store/types.ts
@@ -0,0 +1,88 @@
+export const GET_WEATHER = "GET_WEATHER";
+export const SET_LOADING = "SET_LOADING";
+export const SET_ERROR = "SET_ERROR";
+export const SET_ALERT = "SET_ALERT";
+
+export interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface WeatherData {
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  base: string;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  visibility: number;
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    message: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+  coord: {
+    lon: number;
+    lar: number;
+  };
+  weather: Weather[];
+}
+
+export interface WeatherError {
+  cod: string;
+  message: string;
+}
+
+export interface WeatherState {
+  data: WeatherData | null;
+  loading: boolean;
+  error: string;
+}
+
+interface GetWeatherAction {
+  type: typeof GET_WEATHER;
+  payload: WeatherData;
+}
+
+interface SetLoadingAction {
+  type: typeof SET_LOADING;
+}
+
+interface SetErrorAction {
+  type: typeof Z_STREAM_ERROR;
+  payload: string;
+}
+
+export type WeatherAction =
+  | GetWeatherAction
+  | SetLoadingAction
+  | SetErrorAction;
+
+export interface AlertAction {
+  type: typeof SET_ALERT;
+  payload: string;
+}
+
+export interface AlertState {
+  message: string;
+}
